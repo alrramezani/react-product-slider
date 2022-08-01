007968465ade10f673c5f07c373bf908f17d78dc
Alireza Ramezani <alireza@Alirezas-MacBook-Pro.local>
2022-08-01 09:24:23 +0430

    preparing environment

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import ReactProductSlider from "./react-product-slider";
+function App() {
+  return <ReactProductSlider />;
+}
+export default App;
---
+++ src/react-product-slider/index.tsx
@@ -0,0 +1,8 @@
+
+type ReactProductSliderTypes ={
+    data:{images:string[],thumpnails:string[]}
+}
+const ReactProductSlider = ():JSX.Element=>{
+    return <div></div>
+}
+export default ReactProductSlider;
